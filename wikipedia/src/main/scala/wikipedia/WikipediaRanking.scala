5e9867372154afdf94eb4a0d02979c6e9ec96436
Lodewijk Pool <lodewijk.pool@here.com>
2017-03-19 15:42:05 +0100

    Coursera Big Data Analysis with Scala and Spark - First Assignment

--- wikipedia/src/main/scala/wikipedia/WikipediaRanking.scala
+++ wikipedia/src/main/scala/wikipedia/WikipediaRanking.scala
@@ -14,10 +14,16 @@ object WikipediaRanking {
     "JavaScript", "Java", "PHP", "Python", "C#", "C++", "Ruby", "CSS",
     "Objective-C", "Perl", "Scala", "Haskell", "MATLAB", "Clojure", "Groovy")
 
-  val conf: SparkConf = ???
-  val sc: SparkContext = ???
+  val conf: SparkConf = new SparkConf().setMaster("local").setAppName("Wikipedia Spark App")
+  val sc: SparkContext = new SparkContext(conf)
+
   // Hint: use a combination of `sc.textFile`, `WikipediaData.filePath` and `WikipediaData.parse`
-  val wikiRdd: RDD[WikipediaArticle] = ???
+  val wikiRdd: RDD[WikipediaArticle] = sc.textFile(WikipediaData.filePath).map(s => WikipediaData.parse(s))
+
+  /**
+   * Checks for the language in the article text.
+   */
+  def contains(lang: String, article: WikipediaArticle) = article.text.split(" ").contains(lang)
 
   /** Returns the number of articles on which the language `lang` occurs.
    *  Hint1: consider using method `aggregate` on RDD[T].
@@ -25,7 +31,9 @@ object WikipediaRanking {
    *  Hint3: the only whitespaces are blanks " "
    *  Hint4: no need to search in the title :)
    */
-  def occurrencesOfLang(lang: String, rdd: RDD[WikipediaArticle]): Int = ???
+  def occurrencesOfLang(lang: String, rdd: RDD[WikipediaArticle]): Int = {
+    rdd.aggregate(0)((count, article) => if (contains(lang, article)) count + 1 else count, _ + _)
+  }
 
   /* (1) Use `occurrencesOfLang` to compute the ranking of the languages
    *     (`val langs`) by determining the number of Wikipedia articles that
@@ -35,20 +43,54 @@ object WikipediaRanking {
    *   Note: this operation is long-running. It can potentially run for
    *   several seconds.
    */
-  def rankLangs(langs: List[String], rdd: RDD[WikipediaArticle]): List[(String, Int)] = ???
+  def rankLangs(langs: List[String], rdd: RDD[WikipediaArticle]): List[(String, Int)] = {
+    langs.map(lang => (lang, occurrencesOfLang(lang, rdd))).sortWith(_._2 > _._2)
+  }
 
   /* Compute an inverted index of the set of articles, mapping each language
    * to the Wikipedia pages in which it occurs.
+   *
+   * (on the collect function)
+   * When combining a map and filter one can rely on Partial Functions and Pattern Matching:
+   *
+   * langs.map(_ match {
+   *   case lang contains(lang, article) => Some((lang, article))
+   *   case _ => None
+   * })
+   *
+   * or just use the collect method:
+   *
+   * langs.collect({ case lang if contains(lang, article) => (lang, article) }))
+   *
+   * Given a partial function pf, the following code:
+   * someCollection.collect(pf)
+   *
+   * Is roughly equivalent to :
+   * someCollection.filter(pf.isDefinedAt _).map(pf)
    */
-  def makeIndex(langs: List[String], rdd: RDD[WikipediaArticle]): RDD[(String, Iterable[WikipediaArticle])] = ???
+  def makeIndex(langs: List[String], rdd: RDD[WikipediaArticle]): RDD[(String, Iterable[WikipediaArticle])] = {
+    rdd.flatMap(article => langs.collect({ case lang if contains(lang, article) => (lang, article) })).groupByKey()
+  }
 
   /* (2) Compute the language ranking again, but now using the inverted index. Can you notice
    *     a performance improvement?
    *
    *   Note: this operation is long-running. It can potentially run for
    *   several seconds.
+   *
+   *   (On the use of mapValues)
+   *   mapValues is only applicable for PairRDDs, meaning RDDs of the form RDD[(A, B)]. In that case, mapValues operates on the value only (the second part of the tuple), while map operates
+   *   on the entire record (tuple of key and value).
+   *   In other words, given f: B => C and rdd: RDD[(A, B]], these two are (almost) identical:
+   *
+   *   val result: RDD[(A, C)] = rdd.map { case (k, v) => (k, f(v)) }
+   *   val result: RDD[(A, C)] = rdd.mapValues(f)
+   *
+   *   The latter is shorter and clearer, so when you just want to transform the values and keep the keys as-is, use mapValues.
    */
-  def rankLangsUsingIndex(index: RDD[(String, Iterable[WikipediaArticle])]): List[(String, Int)] = ???
+  def rankLangsUsingIndex(index: RDD[(String, Iterable[WikipediaArticle])]): List[(String, Int)] = {
+    index.mapValues(articles => articles.size).sortBy(_._2, ascending = false).collect().toList
+  }
 
   /* (3) Use `reduceByKey` so that the computation of the index and the ranking is combined.
    *     Can you notice an improvement in performance compared to measuring *both* the computation of the index
@@ -57,7 +99,9 @@ object WikipediaRanking {
    *   Note: this operation is long-running. It can potentially run for
    *   several seconds.
    */
-  def rankLangsReduceByKey(langs: List[String], rdd: RDD[WikipediaArticle]): List[(String, Int)] = ???
+  def rankLangsReduceByKey(langs: List[String], rdd: RDD[WikipediaArticle]): List[(String, Int)] = {
+    rdd.flatMap(article => langs.collect({ case lang if contains(lang, article) => (lang, 1) })).reduceByKey(_ + _).sortBy(_._2, ascending = false).collect().toList
+  }
 
   def main(args: Array[String]) {
 
--- wikipedia/src/test/scala/wikipedia/WikipediaSuite.scala
+++ wikipedia/src/test/scala/wikipedia/WikipediaSuite.scala
@@ -44,14 +44,53 @@ class WikipediaSuite extends FunSuite with BeforeAndAfterAll {
     assert(res, "occurrencesOfLang given (specific) RDD with one element should equal to 1")
   }
 
+  test("'occurrencesOfLang' should work for (specific) RDD with more than one element element") {
+    assert(initializeWikipediaRanking(), " -- did you fill in all the values in WikipediaRanking (conf, sc, wikiRdd)?")
+    import WikipediaRanking._
+    val rdd = sc.parallelize(Seq(WikipediaArticle("1", "Java Jakarta is not actually a Programming Language"),
+                                 WikipediaArticle("2", "Scala is a mixed approach to functional programming"),
+                                 WikipediaArticle("3", "Spark is awesome, but Flink does Streaming a bit better"),
+                                 WikipediaArticle("4", "JavaScript is an interpreted language, and one can argue the same for all JVM-based languages such as Scala"),
+                                 WikipediaArticle("5", "Unfortunately, Scala is a bit slow if you always follow functional principles")))
+
+    assert((occurrencesOfLang("Java", rdd) == 1))
+    assert((occurrencesOfLang("Scala", rdd) == 3))
+  }
+
   test("'rankLangs' should work for RDD with two elements") {
     assert(initializeWikipediaRanking(), " -- did you fill in all the values in WikipediaRanking (conf, sc, wikiRdd)?")
     import WikipediaRanking._
     val langs = List("Scala", "Java")
-    val rdd = sc.parallelize(List(WikipediaArticle("1", "Scala is great"), WikipediaArticle("2", "Java is OK, but Scala is cooler")))
+    val rdd = sc.parallelize(List(WikipediaArticle("1", "Scala is great, Python is pretty neat too, and more widely adopted"),
+                                  WikipediaArticle("2", "Java is a brilliant language, Scala can be too")))
     val ranked = rankLangs(langs, rdd)
     val res = ranked.head._1 == "Scala"
     assert(res)
+  }
+
+  test("'rankLangs' should work for RDD with more than two elements") {
+    assert(initializeWikipediaRanking(), " -- did you fill in all the values in WikipediaRanking (conf, sc, wikiRdd)?")
+    import WikipediaRanking._
+    val langs = List("Scala", "Java", "JavaScript", "Python", "Occam")
+    val rdd = sc.parallelize(List(WikipediaArticle("1", "Scala is great, Python is pretty neat too, and more widely adopted"),
+                                  WikipediaArticle("2", "Java is a brilliant language, Scala can be too"),
+                                  WikipediaArticle("3", "Java Jakarta is not actually a Programming Language"),
+                                  WikipediaArticle("4", "Scala is a mixed approach to functional programming"),
+                                  WikipediaArticle("5", "Spark is awesome, but Flink does Streaming a bit better"),
+                                  WikipediaArticle("6", "JavaScript is an interpreted language, and one can argue the same for all JVM-based languages such as Scala"),
+                                  WikipediaArticle("7", "Unfortunately, Scala is a bit slow if you always follow functional principles")))
+    val ranked = rankLangs(langs, rdd)
+
+    assert(ranked.head._1 === "Scala")
+    assert(ranked.head._2 === 5)
+    assert(ranked.tail.head._1 === "Java")
+    assert(ranked.tail.head._2 === 2)
+    assert(ranked.tail.tail.head._1 === "JavaScript")
+    assert(ranked.tail.tail.head._2 === 1)
+    assert(ranked.tail.tail.tail.head._1 === "Python")
+    assert(ranked.tail.tail.tail.head._2 === 1)
+    assert(ranked.tail.tail.tail.tail.head._1 === "Occam")
+    assert(ranked.tail.tail.tail.tail.head._2 === 0)
   }
 
   test("'makeIndex' creates a simple index with two entries") {
@@ -66,6 +105,26 @@ class WikipediaSuite extends FunSuite with BeforeAndAfterAll {
     val rdd = sc.parallelize(articles)
     val index = makeIndex(langs, rdd)
     val res = index.count() == 2
+    assert(res)
+  }
+
+  test("'makeIndex' creates a simple index with more than two entries") {
+    assert(initializeWikipediaRanking(), " -- did you fill in all the values in WikipediaRanking (conf, sc, wikiRdd)?")
+    import WikipediaRanking._
+    val langs = List("Scala", "Java", "Groovy", "Erlang", "JavaScript")
+    val articles = List(WikipediaArticle("1", "Groovy is pretty interesting, and so is Erlang"),
+                        WikipediaArticle("2", "Scala and Java run on the JVM"),
+                        WikipediaArticle("3", "Scala is not purely functional"),
+                        WikipediaArticle("4", "Scala is great, Python is pretty neat too"),
+                        WikipediaArticle("6", "Java is a brilliant language, Scala can be too"),
+                        WikipediaArticle("7", "Java Jakarta is not actually a Programming Language"),
+                        WikipediaArticle("8", "Scala is a practical approach to functional programming"),
+                        WikipediaArticle("9", "Spark is awesome, but Flink does Streaming a bit better"),
+                        WikipediaArticle("10","JavaScript is an interpreted language, and one can argue the same for all JVM-based languages such as Scala"),
+                        WikipediaArticle("11","Unfortunately, Scala is a bit slow if you always follow functional principles"))
+    val rdd = sc.parallelize(articles)
+    val index = makeIndex(langs, rdd)
+    val res = index.count() == 5
     assert(res)
   }
 
@@ -85,6 +144,40 @@ class WikipediaSuite extends FunSuite with BeforeAndAfterAll {
     assert(res)
   }
 
+  test("'rankLangsUsingIndex' should work for a simple RDD with more than three elements") {
+    assert(initializeWikipediaRanking(), " -- did you fill in all the values in WikipediaRanking (conf, sc, wikiRdd)?")
+    import WikipediaRanking._
+    val langs = List("Scala", "Java", "JavaScript", "Erlang", "Groovy", "Occam", "Python")
+    val articles = List(WikipediaArticle("1", "Groovy is pretty interesting, and so is Erlang"),
+                        WikipediaArticle("2", "Scala and Java run on the JVM"),
+                        WikipediaArticle("3", "Scala is not purely functional"),
+                        WikipediaArticle("4", "Scala is great, Python is pretty neat too"),
+                        WikipediaArticle("6", "Java is a brilliant language, Scala can be too"),
+                        WikipediaArticle("7", "Java Jakarta is not actually a Programming Language"),
+                        WikipediaArticle("8", "Scala is a practical approach to functional programming"),
+                        WikipediaArticle("9", "Spark is awesome, but Flink does Streaming a bit better"),
+                        WikipediaArticle("10", "JavaScript is an interpreted language, and one can argue the same for all Java Virtual Machine based languages such as Scala"),
+                        WikipediaArticle("11", "Unfortunately, Scala is a bit slow if you always follow functional principles"),
+                        WikipediaArticle("12", "We don't talk about Python in this course"))
+    val rdd = sc.parallelize(articles)
+    val index = makeIndex(langs, rdd)
+    val ranked = rankLangsUsingIndex(index)
+    assert(ranked.head._1 === "Scala")
+    assert(ranked.head._2 === 7)
+    assert(ranked.tail.head._1 === "Java")
+    assert(ranked.tail.head._2 === 4)
+    assert(ranked.tail.tail.head._1 === "Python")
+    assert(ranked.tail.tail.head._2 === 2)
+    //assert(ranked.tail.tail.tail.head._1 === "JavaScript") //Sorting is not stable
+    assert(ranked.tail.tail.tail.head._2 === 1)
+    //assert(ranked.tail.tail.tail.tail.head._1 === "Erlang")
+    assert(ranked.tail.tail.tail.tail.head._2 === 1)
+    //assert(ranked.tail.tail.tail.tail.tail.head._1 === "Groovy")
+    assert(ranked.tail.tail.tail.tail.tail.head._2 === 1)
+    //assert(ranked.tail.tail.tail.tail.tail.tail.head._1 === "Occam") //Languages not contained in any articles are not added to the RDD
+    //assert(ranked.tail.tail.tail.tail.tail.tail.head._2 === 0)
+  }
+
   test("'rankLangsReduceByKey' should work for a simple RDD with four elements") {
     assert(initializeWikipediaRanking(), " -- did you fill in all the values in WikipediaRanking (conf, sc, wikiRdd)?")
     import WikipediaRanking._
@@ -93,7 +186,8 @@ class WikipediaSuite extends FunSuite with BeforeAndAfterAll {
         WikipediaArticle("1","Groovy is pretty interesting, and so is Erlang"),
         WikipediaArticle("2","Scala and Java run on the JVM"),
         WikipediaArticle("3","Scala is not purely functional"),
-        WikipediaArticle("4","The cool kids like Haskell more than Java"),
+        WikipediaArticle("4","The cool kids like Haskell more than Java. The ones who want to work and make money write Java and leave Haskell to " +
+                             "rot in the annals of other functional programming attempts"),
         WikipediaArticle("5","Java is for enterprise developers")
       )
     val rdd = sc.parallelize(articles)
